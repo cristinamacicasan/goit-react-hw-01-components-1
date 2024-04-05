78bfc61503797935baadd4c56153b02f60b65d8a
cristinamacicasan <cmacicasan@gmail.com>
2024-04-05 09:58:07 +0300

    finish profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import React from "react";
+
+import Profile from './Profile/Profile';
+import userData from './Profile/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
       React homework template
     </div>
   );
 };
+
+
+
+export default App;
